52ed1c3b1a85957bf9b38a85f078e49e468cb610
MarcusAureliusNV <samuelpalomopacheco@gmail.com>
2026-01-28 12:12:08 +0100

    Flip dog sprite

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED: float  = 500.0
 const JUMP_VELOCITY: float = -600.0
 
+@onready var dog_sprite: Sprite2D = $dog_sprite
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,6 +15,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if velocity.x < 0:
+		dog_sprite.flip_h = true
+	else:
+		dog_sprite.flip_h = false
+	
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
