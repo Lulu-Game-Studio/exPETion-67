cd8d8aa09bceabc26bb588753e50e9b63d28ff01
MarcusAureliusNV <samuelpalomopacheco@gmail.com>
2026-01-29 13:54:51 +0100

    Running applied, also while jump, changed logic of velocity.x and action_pressed of run, added jump falling animation, reworked commentaries.

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -1,64 +1,86 @@
 extends CharacterBody2D
 
-const SPEED: float = 100.0
-const JUMP_VELOCITY: float = -200.0
+const SPEED: float = 50.0
+const SPEED_RUNNING: float = 150.0
+const JUMP_VELOCITY: float = -150.0
 
 @onready var sprite: Sprite2D = %dog_sprite
 @onready var anim: AnimationPlayer = %AnimationDog
 
-var busy: bool = false # Declared outside a function so it saves in the long run
+# Boolean to check if there is another animation queuing to play besides movement ones
+var busy: bool = false 
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():	# Falling gravity
+	if not is_on_floor():	
 		velocity += get_gravity() * delta
+	# Left/Right Input
+	var direction := Input.get_axis("left", "right")
 
 	# Checks if a special animation key is pressed (not busy)
 	if not busy:
 		if Input.is_action_just_pressed("poop") and is_on_floor():
 			velocity = Vector2.ZERO
 			play_special_animation("dog_poop")
-		if Input.is_action_just_pressed("run") and is_on_floor():
-			velocity = Vector2.ZERO
-			play_special_animation("dog_run")
+		# Next two if's passes because there isn't neither the sprite nor the animation to do that
 		if Input.is_action_just_pressed("6") and is_on_floor():
 			pass
 		if Input.is_action_just_pressed("7") and is_on_floor():
 			pass
+			
 		if Input.is_action_just_pressed("bark") and is_on_floor():
 			velocity = Vector2.ZERO
 			play_special_animation("dog_lines")
-			return # Stop reading code here so we don't accidentally play other anims
 
-	# MOVEMENT LOGIC (Only runs if not busy)
+	# Movement Logic (Only runs if not busy, separed from the upper if for more organization)
 	if not busy:
-		# Jump Input
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		
-		# Left/Right Input
-		var direction := Input.get_axis("left", "right")
+		# Local variable to know if it's running or not more below
+		var actual_speed = SPEED
+		# Keeps working until unpressed
+		if Input.is_action_pressed("run"):
+			actual_speed = SPEED_RUNNING
+		
+		# Flipping sprites, works with jumping too
 		if direction:
 			velocity.x = direction * SPEED
+			if velocity.x < 0:
+				sprite.flip_h = true
+			elif velocity.x > 0:
+				sprite.flip_h = false
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		# Movement animations (only play if there is no other animation
+			
+		# It's over the jumping animation so it still runs while jumping
+		velocity.x = actual_speed * direction
+		# Movement animations (only play if there is no other animation)
 		if not is_on_floor():
-			anim.play("dog_jump")
-			# Flip sprite, works both air and ground
-			if velocity.x != 0:
-				sprite.flip_h = velocity.x < 0
+			# y is reversed in godot
+			if velocity.y < 0:
+				anim.play("dog_jump")
+			else:
+				anim.play("dog_fall")
+			# How to apply the ground animation?
+			
+		## Movement animation logic, if the local variable is equal to the absolute quantity 
+		## (the absolute is necessary so left-negative is also covered), plays its own animation, else it's idle
 		elif velocity.x != 0:
-			anim.play("dog_run")
-			sprite.flip_h = velocity.x < 0
+			if actual_speed == abs(SPEED):
+				anim.play("dog_walk")
+			elif actual_speed == abs(SPEED_RUNNING):
+				anim.play("dog_run")
 		else:
 			anim.play("dog_idle")
 
 	move_and_slide()
 
-func play_special_animation(animation_name: String) -> void: # Funtion to stop other animations from stepping on special animations (idle fix)
+## Function to stop the character from being able to move (busy == true), then starts the next animation
+## (the one pressed), which changes its direction to ZERO (0, 0) so it doesn't move itself while animating,
+## ends the animation with the 'await' command and turns busy false so you can play the game again.
+func play_special_animation(animation_name: String) -> void: 
 	busy = true
 	anim.play(animation_name)
-	# Wait until this specific animation finishes
-	await anim.animation_finished # It waits for the animation to end so you can play th game
+	# It waits for the animation to end so you can play the game
+	await anim.animation_finished 
 	busy = false
