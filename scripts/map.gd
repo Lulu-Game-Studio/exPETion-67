8c8677898cad7836eaa69bb2fc4514f23ef025d3
JosemaDev06 <josemaestudiosdam@gmail.com>
2026-02-02 12:19:43 +0100

    fixing poopBar & adding funcional map

---
+++ scripts/map.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/maps/Level01.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/maps/Level02.tscn")
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/maps/Level03.tscn")
+
+func _on_level_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/maps/Level04.tscn")
+
+func _on_level_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/maps/level05.tscn")
+
+func _on_level_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/maps/level06.tscn")
