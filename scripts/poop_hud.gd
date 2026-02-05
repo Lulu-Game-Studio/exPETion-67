04fc87f32ea4c5809b9cfc816ec7b23e2fcf2f82
JosemaDev06 <josemaestudiosdam@gmail.com>
2026-02-05 12:57:27 +0100

    lot of changes

---
+++ scripts/poop_hud.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var label = $Sprite2D/Label
+
+func _process(delta: float):
+	var dog = get_tree().get_first_node_in_group("player")
+	var dogPoop = dog.poopCoins
+	label.text =  str(dogPoop)+"/3"
